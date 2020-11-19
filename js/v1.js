447648bf9fd18f4c006e9847c48a78c43400b2eb
doky007 <peric.d03@htlwienwest.at>
2020-11-19 08:50:07 +0100

    Flip

--- js/v1.js
+++ js/v1.js
@@ -1,6 +1,17 @@
 let c = document.querySelectorAll('.card')
+
 for (const i of c) {
-    i.addEventListener('click',function(event){
-        console.log("klick")
-    })
+    i.addEventListener('click',flipp)
 }
+
+function xy(flipp){
+    this.classList.toggle('flip')
+    //alert("test123")
+}
+
+
+
+for (let index = 0; index < 17; index++) {
+    const newElement = document.createElement(div);
+}
+myElement.classList.toggle('flip')

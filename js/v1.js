1f1b67a2f08a439577ad8e158192f4c0c53699df
doky007 <peric.d03@htlwienwest.at>
2020-11-19 09:12:35 +0100

    .

--- js/v1.js
+++ js/v1.js
@@ -3,7 +3,7 @@ const numberCards = 16
 let p = document.querySelector('.deck')
 for (let index = 0; index < numberCards; index++) {
         let c = document.createElement('div')
-        c.innerHTML = index % 2 + 1
+        c.innerHTML = index % 4 + 1
         c.className="card"
         c.addEventListener('click',flipp)
         p.appendChild(c)
